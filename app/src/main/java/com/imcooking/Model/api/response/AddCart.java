6c3ad7eed301176c5508bf4fdc15748216caa150
AskOnlineSolutions <shaiyad@askonlinesolutions.com>
2018-05-10 19:23:47 +0530

    10/05/2018 7:23 pm Md Shaiyad

---
+++ app/src/main/java/com/imcooking/Model/api/response/AddCart.java
@@ -0,0 +1,117 @@
+package com.imcooking.Model.api.response;
+
+import java.util.List;
+
+public class AddCart {
+
+
+    /**
+     * chef_id : 5
+     * chef_name : brijesh chef
+     * chef_image : 1520577541jobseeker.png
+     * rating : 5
+     * follow : 3
+     * add_dish : [{"addcart_id":9,"dish_id":5,"dish_image":"1520916667kheer.jpg","dish_name":"matter paner"},{"addcart_id":10,"dish_id":6,"dish_image":"1524488071kheer.jpg","dish_name":"india food"}]
+     */
+
+    private int chef_id;
+    private String chef_name;
+    private String chef_image;
+    private int rating;
+    private int follow;
+    private List<AddDishBean> add_dish;
+
+    public int getChef_id() {
+        return chef_id;
+    }
+
+    public void setChef_id(int chef_id) {
+        this.chef_id = chef_id;
+    }
+
+    public String getChef_name() {
+        return chef_name;
+    }
+
+    public void setChef_name(String chef_name) {
+        this.chef_name = chef_name;
+    }
+
+    public String getChef_image() {
+        return chef_image;
+    }
+
+    public void setChef_image(String chef_image) {
+        this.chef_image = chef_image;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getFollow() {
+        return follow;
+    }
+
+    public void setFollow(int follow) {
+        this.follow = follow;
+    }
+
+    public List<AddDishBean> getAdd_dish() {
+        return add_dish;
+    }
+
+    public void setAdd_dish(List<AddDishBean> add_dish) {
+        this.add_dish = add_dish;
+    }
+
+    public static class AddDishBean {
+        /**
+         * addcart_id : 9
+         * dish_id : 5
+         * dish_image : 1520916667kheer.jpg
+         * dish_name : matter paner
+         */
+
+        private int addcart_id;
+        private int dish_id;
+        private String dish_image;
+        private String dish_name;
+
+        public int getAddcart_id() {
+            return addcart_id;
+        }
+
+        public void setAddcart_id(int addcart_id) {
+            this.addcart_id = addcart_id;
+        }
+
+        public int getDish_id() {
+            return dish_id;
+        }
+
+        public void setDish_id(int dish_id) {
+            this.dish_id = dish_id;
+        }
+
+        public String getDish_image() {
+            return dish_image;
+        }
+
+        public void setDish_image(String dish_image) {
+            this.dish_image = dish_image;
+        }
+
+        public String getDish_name() {
+            return dish_name;
+        }
+
+        public void setDish_name(String dish_name) {
+            this.dish_name = dish_name;
+        }
+    }
+}
--- app/src/main/java/com/imcooking/activity/Sub/Cart_activity.java
+++ app/src/main/java/com/imcooking/activity/Sub/Cart_activity.java
@@ -2,14 +2,71 @@ package com.imcooking.activity.Sub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.imcooking.Model.ApiRequest.AddToCart;
+import com.imcooking.Model.api.response.ApiResponse;
 import com.imcooking.R;
+import com.imcooking.webservices.GetData;
 
 public class Cart_activity extends AppCompatActivity {
-
+TextView txtPlus,txtMinus,txt_itemCount,txtDishPrice,txtTax,txtTotalprice,txtChef_Name,txt_chef_follow;
+ImageView imgDish,imgChefImg;
+static int count=0;
+int foodie_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            foodie_id = extras.getInt("foodie_id");
+            // and get whatever type user account id is
+        }
+        txtChef_Name=findViewById(R.id.chef_name);
+
+
+
+
+        setdetails();
     }
+
+    private void setdetails() {
+        AddToCart addToCart=new AddToCart();
+        addToCart.setFoodie_id(foodie_id);
+        new GetData(getApplicationContext(), Cart_activity.this)
+                .getResponse(new Gson().toJson(addToCart), "cart",
+                        new GetData.MyCallback() {
+                            @Override
+                            public void onSuccess(String result) {
+                                final String response = result;
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ApiResponse apiResponse = new Gson().fromJson(response,
+                                                ApiResponse.class);
+
+                                        if(apiResponse.isStatus()){
+
+                                            txtChef_Name.setText(apiResponse.getAdd_cart().getChef_name());
+                                        }
+
+//                                        apiResponse.isStatus();
+
+//                                            Log.d("ShowResponse", apiResponse.getUser_data().toString());
+
+
+
+                                    }
+                                });
+                            }
+                        });
+
+    }
+
 }
