863806453718da0450b75cf90ba00f012f7ca6df
lenovo <rakhi@askonlinesolution.com>
2018-05-14 17:52:17 +0530

    change by rakhi 14/05/2018 5:51 PM

--- app/src/main/java/com/imcooking/activity/Sub/AddAddressActivity.java
+++ app/src/main/java/com/imcooking/activity/Sub/AddAddressActivity.java
@@ -2,11 +2,14 @@ package com.imcooking.activity.Sub;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Handler;
@@ -18,7 +21,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -33,12 +41,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.imcooking.R;
+import com.imcooking.activity.main.setup.LoginActivity;
+import com.imcooking.adapters.PlacesAutoCompleteAdapter;
 import com.imcooking.utils.AppBaseActivity;
 import com.imcooking.utils.AppUtils;
 import com.imcooking.utils.BaseClass;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
@@ -49,6 +64,7 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
     Context mContext;
     private LatLng mCenterLatLong;
     private MapView mMapView;
+    private TextView txtPlaceName, txtLocatName, txtConfirm;
 
 
     @Override
@@ -71,10 +87,16 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
             return;
         }
         mMapView = (MapView) findViewById(R.id.mapView);
-
+        txtLocatName = findViewById(R.id.activity_add_aaddres_txtLocaname);
+        txtPlaceName = findViewById(R.id.activity_add_aaddres_txtLoc);
+        txtConfirm = findViewById(R.id.add_address_btnConfirm);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
-
+        View locationButton = ((View) mMapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+// position on right bottom
+//        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
         mMapView.getMapAsync(this);
         if (checkPlayServices()) {
             // If this check succeeds, proceed with normal processing.
@@ -95,7 +117,6 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
                     @Override
                     public void onClick(DialogInterface paramDialogInterface, int paramInt) {
                         // TODO Auto-generated method stub
-
                     }
                 });
                 dialog.show();
@@ -104,8 +125,36 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
         } else {
             Toast.makeText(mContext, "Location not supported in this device", Toast.LENGTH_SHORT).show();
         }
+        AutoCompleteTextView autocompleteView = (AutoCompleteTextView) findViewById(R.id.autocomplete);
+        autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(getApplicationContext(), R.layout.autocomplete_list_item));
+        autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get data associated with the specified position
+                // in the list (AdapterView)
+                String description = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        txtConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createMyDialog();
+            }
+        });
     }
+    Dialog dialog;
+    private void createMyDialog(){
+
+        dialog = new Dialog(AddAddressActivity.this);
+        dialog.setContentView(R.layout.dialog_save_address);
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawable(null);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.show();
+    }
+
 
     private boolean checkPlayServices() {
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
@@ -144,12 +193,11 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
         if (mMap != null) {
             mMap.getUiSettings().setZoomControlsEnabled(false);
             LatLng latLong;
-
-
             latLong = new LatLng(location.getLatitude(), location.getLongitude());
-
+           /* mMap.addMarker(new MarkerOptions().position(latLong)
+                    .icon(BaseClass.bitmapDescriptorFromVectorR(getApplicationContext())));*/
             CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(latLong).zoom(20f).tilt(60).build();
+                    .target(latLong).zoom(21f).tilt(60).build();
          /*   CircleOptions circleOptions = new CircleOptions()
                     .center(latLong)
                     .strokeWidth(2)
@@ -160,6 +208,7 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
             //   #AA is the alpha, or amount of transparency
 
 //            mMap.addCircle(circleOptions);
+
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
             mMap.animateCamera(CameraUpdateFactory
@@ -175,7 +224,6 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
 
     }
 
-
     @Override
     public void onStart() {
         super.onStart();
@@ -185,6 +233,29 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public StringBuffer getAddress(LatLng latLng) throws IOException {
+        Geocoder geocoder;
+        List<Address> addresses;
+        StringBuffer result = new StringBuffer();
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+            result.append(address);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
@@ -206,7 +277,6 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
                 .addApi(LocationServices.API)
                 .build();
     }
-
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -250,10 +320,7 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -273,7 +340,6 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
                 changeMap(location);
             LocationServices.FusedLocationApi.removeLocationUpdates(
                     mGoogleApiClient, this);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -283,7 +349,8 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         Log.d(TAG, "OnMapReady");
         mMap = googleMap;
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+//        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(false);
 
         mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @SuppressLint("SetTextI18n")
@@ -298,15 +365,19 @@ public class AddAddressActivity extends AppBaseActivity implements OnMapReadyCal
                     Location mLocation = new Location("");
                     mLocation.setLatitude(mCenterLatLong.latitude);
                     mLocation.setLongitude(mCenterLatLong.longitude);
-
-
+                    StringBuffer stringBuffer  = new StringBuffer();
+                    try {
+                        stringBuffer=getAddress(new LatLng(mCenterLatLong.latitude,mCenterLatLong.longitude));
+                        txtPlaceName.setText(stringBuffer);
+                        txtLocatName.setText(stringBuffer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
             }
         });
-
     }
 }
 
