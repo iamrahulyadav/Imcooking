fff48c95a629e63a564b776b82856e0dfc2602e0
askonlinesolutions <info@askonlinesolutions.com>
2018-06-02 19:10:58 +0530

    changes done by VK on 2/6/2018 at 07:07 PM

---
+++ app/src/main/java/com/imcooking/Model/ApiRequest/ModelFoodieRequestADish.java
@@ -0,0 +1,98 @@
+package com.imcooking.Model.ApiRequest;
+
+public class ModelFoodieRequestADish {
+
+    /**
+     * request_chef_id : 72
+     * request_foodie_id : 5
+     * request_dishname : paner
+     * request_status : 1
+     * request_cusine_name : indian
+     * request_quantity : 5
+     * request_date : 30/05/2018
+     * request_time : 11:50
+     * request_note : request dish
+     */
+
+    private String request_chef_id;
+    private String request_foodie_id;
+    private String request_dishname;
+    private String request_status;
+    private String request_cusine_name;
+    private String request_quantity;
+    private String request_date;
+    private String request_time;
+    private String request_note;
+
+    public String getRequest_chef_id() {
+        return request_chef_id;
+    }
+
+    public void setRequest_chef_id(String request_chef_id) {
+        this.request_chef_id = request_chef_id;
+    }
+
+    public String getRequest_foodie_id() {
+        return request_foodie_id;
+    }
+
+    public void setRequest_foodie_id(String request_foodie_id) {
+        this.request_foodie_id = request_foodie_id;
+    }
+
+    public String getRequest_dishname() {
+        return request_dishname;
+    }
+
+    public void setRequest_dishname(String request_dishname) {
+        this.request_dishname = request_dishname;
+    }
+
+    public String getRequest_status() {
+        return request_status;
+    }
+
+    public void setRequest_status(String request_status) {
+        this.request_status = request_status;
+    }
+
+    public String getRequest_cusine_name() {
+        return request_cusine_name;
+    }
+
+    public void setRequest_cusine_name(String request_cusine_name) {
+        this.request_cusine_name = request_cusine_name;
+    }
+
+    public String getRequest_quantity() {
+        return request_quantity;
+    }
+
+    public void setRequest_quantity(String request_quantity) {
+        this.request_quantity = request_quantity;
+    }
+
+    public String getRequest_date() {
+        return request_date;
+    }
+
+    public void setRequest_date(String request_date) {
+        this.request_date = request_date;
+    }
+
+    public String getRequest_time() {
+        return request_time;
+    }
+
+    public void setRequest_time(String request_time) {
+        this.request_time = request_time;
+    }
+
+    public String getRequest_note() {
+        return request_note;
+    }
+
+    public void setRequest_note(String request_note) {
+        this.request_note = request_note;
+    }
+}
