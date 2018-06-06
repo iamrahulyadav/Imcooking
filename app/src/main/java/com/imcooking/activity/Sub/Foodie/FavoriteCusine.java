a7936a825ec9aad5efa4674979f3cdfda1e8f4ed
lenovo <rakhi@askonlinesolution.com>
2018-06-06 15:52:35 +0530

    change by rakhi 06/06/2018 03:51PM

--- app/src/main/java/com/imcooking/activity/Sub/Foodie/FavoriteCusine.java
+++ app/src/main/java/com/imcooking/activity/Sub/Foodie/FavoriteCusine.java
@@ -13,7 +13,6 @@ import com.imcooking.Model.api.response.ChefIloveData;
 import com.imcooking.Model.api.response.CuisineData;
 import com.imcooking.Model.api.response.ModelFoodieFavCuisines;
 import com.imcooking.R;
-import com.imcooking.adapters.AdapterCuisineList;
 import com.imcooking.adapters.AdapterFoodieFavCuisines;
 import com.imcooking.adapters.FavrouiteAdatper;
 import com.imcooking.fragment.foodie.HomeFragment;
@@ -31,6 +30,7 @@ public class FavoriteCusine extends AppBaseActivity {
     RecyclerView recyclerView;
     String foodie_id = HomeFragment.foodie_id+"";
     private List<ChefIloveData.ChefloveBean> chefloveBeanList= new ArrayList<>();
+
     private Gson gson = new Gson();
     private CuisineData cuisineData;
     private AdapterFoodieFavCuisines adapter;
