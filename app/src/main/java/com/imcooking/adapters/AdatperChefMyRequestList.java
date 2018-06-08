d8f568ab5133c0d1ae37eb3ddda6bce2b82e3a1f
AskOnlineSolutions <shaiyad@askonlinesolutions.com>
2018-06-08 18:06:19 +0530

    6:05 pm 8/june/2018

---
+++ app/src/main/java/com/imcooking/adapters/AdatperChefMyRequestList.java
@@ -0,0 +1,95 @@
+package com.imcooking.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.imcooking.Model.api.response.ChefMyorderList;
+import com.imcooking.R;
+import com.imcooking.webservices.GetData;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Muhib.
+ * Contact Number : +91 9796173066
+ */
+public class AdatperChefMyRequestList extends RecyclerView.Adapter<AdatperChefMyRequestList.MyViewHolder> {
+
+//    OnItemClickListenerCategory listener;
+    private Context context;
+    List<ChefMyorderList.MyOrderListBean>list=new ArrayList<>();
+
+
+    public AdatperChefMyRequestList(Context context, List<ChefMyorderList.MyOrderListBean> list) {
+        this.context = context;
+        this.list=list;
+
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView tv_view_response, txtTime, txtAddress,txtTotalAmnt,txtEmail,
+                txtCategory,txtprice, txtfoodiename,txtdishname,txtqty;
+
+public ImageView img;
+        public MyViewHolder(View view) {
+            super(view);
+            txtEmail=view.findViewById(R.id.item_chef_my_request_email);
+            txtCategory =view.findViewById(R.id.item_chef_my_request_food_category);
+            txtTime =view.findViewById(R.id.item_chef_my_request_time);
+            txtAddress =view.findViewById(R.id.item_chef_my_request_address);
+            txtTotalAmnt=view.findViewById(R.id.item_chef_my_request_qty);
+            txtqty=view.findViewById(R.id.fragment_chef_order_list_qty);
+            txtfoodiename =view.findViewById(R.id.item_chef_my_request_name);
+            txtdishname=view.findViewById(R.id.item_chef_my_request_dishName);
+            img=view.findViewById(R.id.item_chef_my_request_profile_img);
+
+    }
+    }
+
+    int row_index=-1;
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_chef_my_request, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        holder.txtTime.setText(list.get(position).getBookdate());
+        holder.txtAddress.setText(list.get(position).getOrder_id()+"");
+        holder.txtdishname.setText(list.get(position).getFoodie_name());
+        holder.txtfoodiename.setText(list.get(position).getFoodie_name());
+        holder.txtprice.setText(list.get(position).getPrice()+"");
+       /* holder.txtTotalAmnt.setText(list.get(position).);
+        holder.txtPaymentMode.setText(list.get(position).);
+        holder.txtqty.setText(list.get(position).);
+        holder.txtEmail.setText(list.get(position).);
+       */ Picasso.with(context).load(GetData.IMG_BASE_URL +
+                list.get(position).getFoodie_image()).into(holder.img);
+
+    /*    holder.tv_view_response.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    */}
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/com/imcooking/fragment/chef/ChefMyOrderListFragment.java
@@ -0,0 +1,119 @@
+package com.imcooking.fragment.chef;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.gson.Gson;
+import com.imcooking.Model.api.response.ApiResponse;
+import com.imcooking.Model.api.response.ChefMyorderList;
+import com.imcooking.R;
+import com.imcooking.adapters.AdatperChefMyOrderList;
+import com.imcooking.utils.CustomLayoutManager;
+import com.imcooking.webservices.GetData;
+import com.mukesh.tinydb.TinyDB;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChefMyOrderListFragment extends Fragment {
+TinyDB tinyDB;
+RecyclerView recyclerView;
+   public ChefMyOrderListFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+         View view= inflater.inflate(R.layout.fragment_chef_order_list, container, false);
+        init(view);
+        return view;
+    }
+    private void init(View view) {
+
+        tinyDB=new TinyDB(getContext());
+        getorderList();
+        recyclerView = view.findViewById(R.id.fragment_chef_order_list_recycler);
+        CustomLayoutManager manager1 = new CustomLayoutManager(getContext()){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+        recyclerView.setLayoutManager(manager1);
+
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+    }
+
+    public void getorderList(){
+
+        String login = tinyDB.getString("login_data");
+        ApiResponse.UserDataBean apiResponse = new ApiResponse.UserDataBean();
+        apiResponse = new Gson().fromJson(login,ApiResponse.UserDataBean.class);
+        String user_id=apiResponse.getUser_id()+"";
+
+        // String s = "{\"chef_id\":" + user_id + "}";
+
+        String s = "{\"chef_id\": 1}";
+        Log.d("Tags",s);
+        try {
+            JSONObject job = new JSONObject(s);
+            new GetData(getContext(), getActivity()).sendMyData(job, "chef_order_list", getActivity(),
+                    new GetData.MyCallback() {
+                        @Override
+                        public void onSuccess(String result) {
+
+                            ChefMyorderList chefMyorderList = new ChefMyorderList();
+
+                            chefMyorderList = new Gson().fromJson(result, ChefMyorderList.class);
+
+                            if(chefMyorderList.isStatus()){
+                              if(!chefMyorderList.getMy_order_list().isEmpty()){
+
+                                    setMyAdapter(chefMyorderList.getMy_order_list());
+
+                                }
+                                else {
+                                    Toast.makeText(getContext(), "order list is empty", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(getContext(), "spmething went wrong", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setMyAdapter(List<ChefMyorderList.MyOrderListBean> list){
+        AdatperChefMyOrderList adatperChefMyOrderList = new AdatperChefMyOrderList(getContext(),
+                getFragmentManager(), list);
+        recyclerView.setAdapter(adatperChefMyOrderList);
+    }
+}
